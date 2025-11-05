91a04753b286c0e585c6389debf810d87054dc67
niko_laus <nik.tarnavsky123@gmail.com>
2025-11-05 17:50:50 +0200

    Setup Cypress with custom config2

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,15 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://www.google.com/',
+    viewportWidth: 1440,
+    viewportHeight: 900,
+    video: false,
+    retries: 1,
+    defaultCommandTimeout: 8000,
+    pageLoadTimeout: 60000,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
   },
 });
